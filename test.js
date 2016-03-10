3bc8a1e1408759d7d20d420e5d25d373e17f9332
Иван Соболев <ivan.sobolev@reg.ru>
2016-03-10 19:15:40 +0300

    ignore file and options added, regex better matching

--- test.js
+++ test.js
@@ -5,56 +5,67 @@
 
 "use strict";
 
-var Message = require('./lib/message'),
+var fs = require("fs"),
+    Message = require('./lib/message'),
     rules = require('./lib/rules'),
+    bemlint = require('./lib/bemlint'),
     chai = require('chai'),
     expect = chai.expect,
-    assert = chai.assert,
     should = chai.should();
 
 describe('Check rules', function(){
     var options = { elem: '__', mod: '_' };
 
     for ( var i in rules ) {
-        var rule = rules[i],
-            actual = new Message(rule, options);
+        var rule = rules[i];
 
-        actual.should.be.a('object');
-
-        actual.rule.should.be.a('object');
+        rule.should.be.a('object');
 
         it(i + ' rule has one of [isBlock, isBlockMod, isElem, isElemMod] conditions', function() {
-            actual.rule.should.to.have.any.keys('isBlock', 'isBlockMod', 'isElem', 'isElemMod');
+            rule.should.to.have.any.keys('isBlock', 'isBlockMod', 'isElem', 'isElemMod');
         });
 
-        it(i + ' .valid() exists and is a function', function() {
-             actual.rule.valid.should.be.a('function').and.exist;
+        it(i + '.valid() exists and is a function', function() {
+            rule.valid.should.be.a('function').and.exist;
         });
 
-        it(i + ' .message exists', function() {
-            actual.rule.message.should.exist;
+        it(i + '.message exists', function() {
+            rule.message.should.exist;
         });
     }
 });
 
-describe('Message constructor', function() {
+describe('bemlint.verify(text) should return an array', function() {
 
-    var options = { input: 'b-block', source: ['b-block_mod', 'b-block_mod_value2']},
-        rule = rules.dublicate,
-        actual = new Message(rule, options);
+    var actual = bemlint.verify(fs.readFileSync('./example.html', "utf8"));
 
-    it('.getMessage() should return string', function() {
-        var text = actual.getMessage();
-        expect(text).to.be.a('string');
-    });
 
-    it('should contain options.input String', function() {
-        actual.options.input.should.be.a('string');
-    });
+    expect(actual).to.be.a('array');
 
-    it('should contain options.source Array', function() {
-        actual.options.source.should.be.an('Array');
-    });
+    for ( var i = 0, l = actual.length; i < l; i++ ) {
+        var message = actual[i];
+
+        it('message ' + message + ' should be an object', function() {
+            message.should.be.a('object');
+        });
+
+        it('{}.message should be a string', function() {
+            message.message.should.be.a('string');
+        });
+
+        it('{}.line = ' + (message.line) + ' should be a number', function() {
+            message.line.should.be.a('number');
+        });
+
+        it('{}.column = ' + (message.column) + ' should be a number', function() {
+            message.column.should.be.a('number');
+        });
+
+        it('{}.ruleId = ' + (message.ruleId) + ' should be a string', function() {
+            message.ruleId.should.be.a('string');
+        });
+
+    }
 
 });
 
